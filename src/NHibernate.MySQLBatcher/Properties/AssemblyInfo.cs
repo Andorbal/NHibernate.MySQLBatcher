2652101abbb46084cd6276161bba9710e6f22642
Andrew Benz <andorbal@gmail.com>
2011-10-13 08:20:02 -0500

    Updating the copyright to include Oleg, who wrote the code.

--- src/NHibernate.MySQLBatcher/Properties/AssemblyInfo.cs
+++ src/NHibernate.MySQLBatcher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Andrew Benz")]
 [assembly: AssemblyProduct("NHibernate.MySQLBatcher")]
-[assembly: AssemblyCopyright("Copyright © Andrew Benz 2011")]
+[assembly: AssemblyCopyright("Copyright © Oleg Sakharov, Andrew Benz 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
